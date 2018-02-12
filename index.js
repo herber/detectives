016304484d26dbd670e9e106e953092f27a77701
Tobias Herber <40146720150275@litec.ac.at>
2018-02-12 11:35:00 +0100

    add case for not found

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const regex = new RegExp(requireRegex().source, 'gm');
 module.exports = str => {
   const matches = str.match(regex);
 
+  if (!Array.isArray(matches)) return [];
+
   return matches.map(el => {
     const req = requireRegex().exec(el);
 
